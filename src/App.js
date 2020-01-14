8a09283b68594af6c6b15c3d72a8b516e235bfc8
Seth Lachman <lachmanseth17@gmail.com>
2020-01-14 18:53:10 -0500

    Added react router dom and basic route.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Header from "./components/Header";
 import BreweriesList from "./components/BreweriesList";
 
 function App() {
   return (
-    <Layout>
-      <Header />
-      <BreweriesList />
-    </Layout>
+    <Router>
+      <Layout>
+        <Header />
+        <Switch>
+          <Route path="/">
+            <BreweriesList />
+          </Route>
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
 
